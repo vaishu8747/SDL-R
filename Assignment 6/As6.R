f95635852390c67902760af70c0df42751e43dc3
vaishu8747 <64781305+vaishu8747@users.noreply.github.com>
2020-12-24 09:20:02 -0800

    Add files via upload

---
+++ Assignment 6/As6.R
@@ -0,0 +1,15 @@
+# Assignment 6
+
+install.packages("caret")
+library(caret)
+
+data(Cars)
+View(Cars)
+
+getwd()
+setwd("C:/Users/User/Documents/R") 
+dir()
+data <- read.csv("Cars.csv",header = T) 
+data
+data <- read.csv("Cars.csv",header = T) 
+data
